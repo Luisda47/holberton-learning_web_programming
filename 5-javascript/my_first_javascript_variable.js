840cfce89fd16d861134a81dec37e7e556341231
Luisda47 <58444322+Luisda47@users.noreply.github.com>
2019-12-06 15:50:55 -0500

    Update my_first_javascript_variable.js

--- 5-javascript/my_first_javascript_variable.js
+++ 5-javascript/my_first_javascript_variable.js
@@ -1,2 +1,10 @@
 let my_variable = I love Javascript!
 console.log("my_variable");
+
+  console.log('Comienzo del programa');
+  var contador = 0;
+  while (contador <= 10) {
+    console.log('La variable contador es igual a: ' + contador);
+    contador++;
+  }
+  console.log('Final del programa');
